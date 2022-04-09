03076c7a0bc63b08d63ce5f5f75994e093f39cae
Norm Copeland <norm@goodmanrobot.com>
2022-04-09 17:52:52 -0400

    epoch date fix

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -76,7 +76,7 @@ export const localeAwareUpperCase = (text: string) => {
 
 export const getWordOfDay = () => {
   // April 9 Game Epoch
-  const epochMs = new Date(2022, 4, 9).valueOf()
+  const epochMs = new Date(2022, 3, 9).valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
